5b0f7589aefe57cfb43f75fa6bcfbac853a169b8
Lemuel Marques <39345033+Lemuelmarques@users.noreply.github.com>
2018-05-23 14:04:41 -0300

    GAME CARD

---
+++ cardgame/offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1527094256,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/logo-sheet0.png",
+		"images/play-sheet0.png",
+		"images/fade-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/ideck-sheet0.png",
+		"images/icard-sheet0.png",
+		"images/icard-sheet1.png",
+		"images/spritefont2.png",
+		"images/sensor-sheet0.png",
+		"images/home-sheet0.png",
+		"images/bsound-sheet0.png",
+		"images/bsound-sheet1.png",
+		"images/bmusic-sheet0.png",
+		"images/bmusic-sheet1.png",
+		"images/money-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
