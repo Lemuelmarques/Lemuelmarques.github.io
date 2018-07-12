3d728ae3a6ee4d9f08176d1d715fedf2a0e14bcf
Lemuel Marques <39345033+Lemuelmarques@users.noreply.github.com>
2018-07-12 13:15:46 -0300

    Add files via upload

--- WORLDBLOCKS/offline.js
+++ WORLDBLOCKS/offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1531409860,
+	"version": 1531412043,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -31,7 +31,7 @@
 		"images/gameover.png",
 		"images/backgroundtile2.png",
 		"images/sprite-sheet0.png",
-		"images/sprite2-sheet0.png",
+		"images/falsa-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
